f014838a647ab99722f5b464a53fb4809f325439
alirazabhatti36 <razabhatti131@gmail.com>
2026-09-25 00:31:54 +0500

    chore: remove temporary scratch directory from repo

--- scratch/check_canonical.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const c2 = fs.readFileSync('converter.html', 'utf8');
-const match = c2.match(/<link rel="canonical"[^>]+>/);
-console.log('converter.html canonical:', match ? match[0] : 'None');
-const h2 = fs.readFileSync('hr-helper.html', 'utf8');
-const matchH = h2.match(/<link rel="canonical"[^>]+>/);
-console.log('hr-helper.html canonical:', matchH ? matchH[0] : 'None');
